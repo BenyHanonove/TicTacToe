2d95e1588270c60e4fce5db276b835936fe39203
benyx13 <benyyoulove@gmail.com>
2023-05-15 18:06:47 +0300

    start of client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter ,Route ,Routes} from "react-router-dom";
+import IndexPage from "./pages/IndexPage.js";
+import SearchPage from './pages/SearchPage.js';
+import GamePage from './pages/GamePage.js';
+import WinPage from './pages/WinPage.js';
+import LosePage from './pages/LosePage.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<IndexPage/>}/>
+        <Route path='/search' element={<SearchPage/>}/>
+        <Route path='/game' element={<GamePage/>}/>
+        <Route path='/win' element={<WinPage/>}/>
+        <Route path='/lose' element={<LosePage/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
